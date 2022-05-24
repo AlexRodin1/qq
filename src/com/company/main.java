bec80e27c737c9d22619e37dd398ec078840a9b0
AlexRodin1 <alexrodin.it@gmail.com>
2022-05-24 23:15:44 +0300

    Initial commit

---
+++ src/com/company/main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+
+          User user = new User(name: "Alex1", age: 30);
+        System.out.println(user);
+
+        User user1 = new User(name: "Alex2", age: 27);
+        System.out.println(user1);
+
+        System.out.println(user.equals(user1));
+
+
+
+
+
+   }
+
+
+}
